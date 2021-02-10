0c64a2c118023368cc9cffec889de81c3a7460d8
thisiswhatididwheniwasbored <60962120+thisiswhatididwheniwasbored@users.noreply.github.com>
2021-02-10 11:33:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function compute()
     
     //calculate interest formula
     interest = ((principal * years * rate)/100);
-    amount = (principal + interest);
+    amount = parseInt(principal) + parseInt(interest);
     
     //calculate future year by getting current year and adding number of years
     currentYear = new Date().getFullYear();
-    futureYear = (currentYear + years);
+    futureYear = parseInt(currentYear) + parseInt(years);
 
     r = document.getElementById("result");
     r.innerText = 
