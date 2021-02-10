39cf43111a32e674c2f3ab67972a64777f895b30
thisiswhatididwheniwasbored@outlook.com <thisiswhatididwheniwasbored@outlook.com>
2021-02-10 17:42:01 +0000

    updated all!

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //declare variables and source them from HTML dom
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
     
+    //calculate interest formula
+    interest = ((principal * years * rate)/100);
+    
+    //calculate future year by getting current year and adding number of years
+    currentYear = new Date().getFullYear();
+    futureYear = currentYear + years;
+
+}
+
+
+//function to update Rate range on span 
+function updateRate() {
+    rateValue = document.getElementById("rate-value")
+    rateValue.innerText = document.getElementById("rate").value;
 }
         
